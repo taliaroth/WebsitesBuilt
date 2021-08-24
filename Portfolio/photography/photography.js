187a442b3e61cbc1a14c79663b45dc0d30cf819f
taliaroth <59403502+taliaroth@users.noreply.github.com>
2021-08-24 17:34:58 -0400

    Made website responsive

--- Portfolio/photography/photography.js
+++ Portfolio/photography/photography.js
@@ -1,5 +1,10 @@
 
+document.addEventListener('load', function() {
+    var scale = 1 / (window.devicePixelRatio || 1);
+    var content = 'width=device-width, initial-scale=' + scale;
 
+    document.querySelector('meta[name="viewport"]').setAttribute('content', content)
+}, false)
 
 
 function openModal() {
